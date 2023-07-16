8be7e3b00f9f3ce24a1011aeb865eea523207377
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-07-16 23:21:01 +0300

    Added ground check and dust

--- Assets/Game/Scripts/Player/Dust.cs
+++ Assets/Game/Scripts/Player/Dust.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dust : MonoBehaviour {
-    private void Start() {
-        Hide();
-    }
-
     public void Show() {
         gameObject.SetActive(true);
     }
--- Assets/Game/Scripts/Player/Player.cs
+++ Assets/Game/Scripts/Player/Player.cs
@@ -1,40 +1,62 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private float horizontal;
     private bool isFacingRight = true;
+    private bool isGrounded = true;
     private Rigidbody2D rb;
+    private Dust dust;
 
-    [SerializeField] private float jumpingPower = 16f;
-    [SerializeField] private float speed = 1f;
-    [SerializeField] private GameInput gameInput;
-    [SerializeField] private Transform groundCheck;
+    [Header("Values")]
+    [SerializeField] private float jumpingPower = 10f;
+    [SerializeField] private float speed = 4f;
+
+    [Header("Ground Detection")]
+    [SerializeField] private Vector2 checkSphereRadius;
+    [SerializeField] private Transform feet;
     [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontal = gameInput.GetMovement();
-        Flip();
-    }
-    private void Start()
-    {
+    [Header("Input")]
+    [SerializeField] private GameInput gameInput;
+
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        dust = GetComponentInChildren<Dust>();
+        dust.Hide();
     }
-    private void FixedUpdate()
-    {
+
+    void Update() {
+        Flip();
+        ShowDust();
+        horizontal = gameInput.GetMovement();
+        IsWalking = horizontal != 0f;
+    }
+
+    private void FixedUpdate() {
+        isGrounded = Physics2D.OverlapBox(feet.position, checkSphereRadius, 0f, groundLayer);
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    private void Flip()
-    {
-        if ((isFacingRight && horizontal < 0f) || (!isFacingRight && horizontal > 0f))
-        {
+    private void Flip() {
+        if ((isFacingRight && horizontal < 0f) || (!isFacingRight && horizontal > 0f)) {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
     }
+
+    private void ShowDust() {
+        if (IsWalking && isGrounded) {
+            dust.Show();
+        } else {
+            dust.Hide();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(feet.position, checkSphereRadius);
+    }
+
+    public bool IsWalking { get; private set; }
 }
