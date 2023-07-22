6a184baf568642837f010135c34aa84b81253459
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-07-22 19:29:24 +0300

    Tutorial Level

--- Assets/Game/Scripts/Player/IceSliding.cs
+++ Assets/Game/Scripts/Player/IceSliding.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IceSliding : MonoBehaviour {
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.GetComponent<Ice>() == null) return;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetComponent<Ice>() == null) return;
 
         IsOnIce = true;
     }
 
-    private void OnCollisionExit2D(Collision2D other) {
-        if (other.gameObject.GetComponent<Ice>() == null) return;
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.GetComponent<Ice>() == null) return;
 
         IsOnIce = false;
     }
