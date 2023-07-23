9425575d7a90c1b0512b1714fbbdb33a022e96df
DarkenSoda <mahmoudadel200190@yahoo.com>
2023-07-23 03:51:05 +0300

    Level 2

--- Assets/Game/Scripts/Environment/SnowBall.cs
+++ Assets/Game/Scripts/Environment/SnowBall.cs
@@ -3,20 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowBall : MonoBehaviour
-{
-    [SerializeField] private LevelCompleteCanvasUI levelCompleteCanvasUI;
+public class SnowBall : MonoBehaviour {
     public static event EventHandler OnSnowCollection;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
             this.gameObject.SetActive(false);
             SnowCollected();
-            Destroy(this.gameObject,2f);
+            Destroy(this.gameObject, 2f);
         }
     }
     private void SnowCollected() {
-        OnSnowCollection?.Invoke(this,EventArgs.Empty);
+        OnSnowCollection?.Invoke(this, EventArgs.Empty);
     }
-    
+
 }
